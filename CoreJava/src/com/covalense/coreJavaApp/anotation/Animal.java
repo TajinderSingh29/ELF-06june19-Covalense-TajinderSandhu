d633051fc645ceb763e968943caf37f66594a39d
TajinderSingh <singhtajinder.s1995@gmail.com>
2019-06-21 16:14:18 +0530

    Removing code changes

--- CoreJava/src/com/covalense/coreJavaApp/anotation/Animal.java
+++
@@ -1,8 +0,0 @@
-package com.covalense.corejavaapp.anotation;
-
-public class Animal {
-	void eat() {
-		System.out.println("Animal eat() method is call");
-	}
-
-}
--- CoreJava/src/com/covalense/coreJavaApp/anotation/Cow.java
+++
@@ -1,8 +0,0 @@
-package com.covalense.corejavaapp.anotation;
-
-public class Cow extends Animal {
-	@Override
-	void eat() {
-		System.out.println("Cow eat() method is call");
-	}
-}
